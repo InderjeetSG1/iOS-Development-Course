e3018ad08ea59535fb050ad58da77a8a720062d7
InderjeetSG1 <inderjeetgawra@gmail.com>
2023-03-09 20:30:34 -0800

    First Projects

---
+++ I am Rich/I am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by Inderjeet Singh on 2/7/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
